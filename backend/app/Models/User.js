5b7ce537b74605146d79be9941ebd18913f3039b
Malak Atieh <malak.atieh12@gmail.com>
2025-05-04 12:19:32 +0300

    edited user model

--- backend/app/Models/User.js
+++ backend/app/Models/User.js
@@ -4,21 +4,21 @@ const userSchema = new mongoose.Schema({
   username: { 
     type: String, 
     unique: true, 
-    required: true
+    required: true,
+    trim: true
   },
   email: { 
     type: String, 
     unique: true, 
     required: true, 
+    lowercase: true,
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Invalid email']
   },
   password: { 
     type: String, 
     required: true 
   }, 
-  profilePicture: { type: String }, //base64 from front
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
-});
+  profilePicture: { type: String } //base64 from front
+},{ timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
