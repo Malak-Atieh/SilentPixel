32601d37fd49cf139d87fd09bc72562d5c78d641
Malak Atieh <malak.atieh12@gmail.com>
2025-05-04 16:27:30 +0300

    edited ml services

--- backend/app/Services/MlService.js
+++ backend/app/Services/MlService.js
@@ -1,14 +1,16 @@
 const axios = require('axios');
 
-async function callPythonML({ action, image, message, password }) {
+async function callPythonML({ action, image, message, password, watermark, generateQR }) {
   const formData = new FormData();
   formData.append('image', image, 'image.png');
   if (message) formData.append('message', message);
   if (password) formData.append('password', password);
+  if (watermark) formData.append('watermark', watermark);
+  if (generateQr) formData.append('generateQr', generateQr);
 
   const endpoint = action === 'encode' ? '/encode' : '/decode';
 
-  const response = await axios.post(`http://localhost:5000${endpoint}`, formData, {
+  const response = await axios.post(`http://localhost:5000/api/${endpoint}`, formData, {
     headers: formData.getHeaders()
   });
 
