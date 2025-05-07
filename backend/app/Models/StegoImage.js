0fa6056f57c84ea5afc368b208b20dc97e3ac38e
Malak Atieh <malak.atieh12@gmail.com>
2025-05-07 10:35:19 +0300

    edited attribute structure of model

---
+++ backend/app/Models/StegoImage.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+
+const stegoImageSchema = new mongoose.Schema({
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "User", 
+    required: true 
+  },
+  originalImageUrl: { 
+    type: String, 
+    required: true 
+  },
+  encodedUrl: { type: String, required: true },
+  watermarkText: { type: String },
+  generateQR: { type: Boolean, default: false },
+  message: { type: String, required: true },
+  type: { 
+    type: String, 
+    enum: ['encode', 'decode'], 
+    required: true 
+  },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("StegoImage", stegoImageSchema);
