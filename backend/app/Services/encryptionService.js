cb050c9791411758847fbbc7608f83485b067844
Malak Atieh <malak.atieh12@gmail.com>
2025-05-08 09:47:49 +0300

    edited encrypt function for more secure encryption

--- backend/app/Services/encryptionService.js
+++ backend/app/Services/encryptionService.js
@@ -2,14 +2,15 @@ const crypto = require('crypto');
 
 class EncryptionService {
   static encrypt(message, password) {
-    const key = crypto.scryptSync(password, 'salt', 32);
+    const salt = crypto.randomBytes(16);
+    const key = crypto.scryptSync(password, salt, 32);
     const iv = crypto.randomBytes(16);
     const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
     
     let encrypted = cipher.update(message, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     
-    return iv.toString('hex') + ':' + encrypted;
+    return salt.toString('hex') + iv.toString('hex') + encrypted;
   }
 
   static decrypt(encryptedMsg, password) {
