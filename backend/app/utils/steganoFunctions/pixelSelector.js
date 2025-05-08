a44b81bb11519603c726cf08468999f58fabe638
Malak Atieh <malak.atieh12@gmail.com>
2025-05-08 09:24:17 +0300

    added fillRemainingIndices to pixelSelector utils

--- backend/app/utils/steganoFunctions/pixelSelector.js
+++ backend/app/utils/steganoFunctions/pixelSelector.js
@@ -33,7 +33,28 @@ class PixelSelector {
         return busyPixels;
       }
     
-
+      static _fillRemainingIndices(indices, totalPixels, busyPixels, dataLength) {
+        let currentIndex = 0;
+        while (indices.length < dataLength && currentIndex < totalPixels) {
+          if (!busyPixels.has(currentIndex)) {
+            indices.push(currentIndex);
+          }
+          currentIndex++;
+        }
+        
+        if (indices.length < dataLength) {
+          throw new Error('Not enough non-busy pixels to embed full message.');
+        }
+      }
+    
+      static _shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+      }
+    
 }
   
   module.exports = PixelSelector;
