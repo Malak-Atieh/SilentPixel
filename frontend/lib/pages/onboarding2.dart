500d968615cdafe664f95a85a5b5a0bca2c50edd
Malak Atieh <malak.atieh12@gmail.com>
2025-05-09 15:32:40 +0300

    added login page

--- frontend/lib/pages/onboarding2.dart
+++ frontend/lib/pages/onboarding2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/pages/onboarding3.dart';
 
 class OnboardingScreenSecond extends StatelessWidget {
   const OnboardingScreenSecond({super.key});
