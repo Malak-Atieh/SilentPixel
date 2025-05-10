ae3c6775fdefa8f135230577afc60529f4555cb8
Malak Atieh <malak.atieh12@gmail.com>
2025-05-10 11:22:16 +0300

    added login import

--- frontend/lib/pages/signUp.dart
+++ frontend/lib/pages/signUp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:frontend/pages/login.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
