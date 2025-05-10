5fd30fc1875929539bd8a355b3b0ab75dfc23733
Malak Atieh <malak.atieh12@gmail.com>
2025-05-10 11:04:52 +0300

    added return scaffold

--- frontend/lib/pages/signUp.dart
+++ frontend/lib/pages/signUp.dart
@@ -3,4 +3,15 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Stack(
+          children: [
+          ],
+      ),
+    );
+  }
 }
