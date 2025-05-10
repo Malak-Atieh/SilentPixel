c1ab799bb9e19a3b3c29ee7cac7858a43d95402e
Malak Atieh <malak.atieh12@gmail.com>
2025-05-10 23:35:55 +0300

    added contructor

---
+++ frontend/lib/pages/encode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Encode extends StatelessWidget {
+  const Encode({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
