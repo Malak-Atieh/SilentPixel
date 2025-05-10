c747452d2bfd7f8eef8662ef87845e0e39565cec
Malak Atieh <malak.atieh12@gmail.com>
2025-05-10 21:03:18 +0300

    added bg color

--- frontend/lib/pages/home.dart
+++ frontend/lib/pages/home.dart
@@ -5,7 +5,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: const Color(0xFF09192C),
     );
   }
 }
