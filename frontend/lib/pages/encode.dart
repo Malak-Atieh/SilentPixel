0299c1249ae25e1ee64a0c20b45339cc1cce5a29
Malak Atieh <malak.atieh12@gmail.com>
2025-05-11 21:34:12 +0300

    edited spacing

--- frontend/lib/pages/encode.dart
+++ frontend/lib/pages/encode.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-
-
 class HideMessageScreen extends StatefulWidget {
   @override
   _HideMessageScreenState createState() => _HideMessageScreenState();
