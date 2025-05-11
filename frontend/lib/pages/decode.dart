bc2c8751c1778cb529f3f89c24321964fefc5bdb
Malak Atieh <malak.atieh12@gmail.com>
2025-05-11 21:47:42 +0300

    added stateful class

--- frontend/lib/pages/decode.dart
+++ frontend/lib/pages/decode.dart
@@ -1,8 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class Decode extends StatelessWidget {
-  const Decode({super.key});
+class RevealMessageScreen extends StatefulWidget {
+  const RevealMessageScreen({super.key});
 
+  @override
+  State<RevealMessageScreen> createState() => _RevealMessageScreenState();
+}
+
+class _RevealMessageScreenState extends State<RevealMessageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
