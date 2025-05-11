c150b69fba1fa9a6519c37fb6953f6e6ceed744c
Malak Atieh <malak.atieh12@gmail.com>
2025-05-11 21:48:19 +0300

    added needed variables

--- frontend/lib/pages/decode.dart
+++ frontend/lib/pages/decode.dart
@@ -11,6 +11,13 @@ class RevealMessageScreen extends StatefulWidget {
 }
 
 class _RevealMessageScreenState extends State<RevealMessageScreen> {
+  final TextEditingController _passwordController = TextEditingController();
+  File? _selectedImage;
+  bool _isAnalyzing = false;
+  bool _hasResult = false;
+  String? _extractedMessage;
+  String? _errorMessage;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
