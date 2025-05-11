04db0e07e86a8c0ca9ca287ae31c3a27dfcad6ee
Malak Atieh <malak.atieh12@gmail.com>
2025-05-11 14:59:04 +0300

    added profile in appbar

--- frontend/lib/pages/encode.dart
+++ frontend/lib/pages/encode.dart
@@ -74,6 +74,15 @@ class _HideMessageScreenState extends State<HideMessageScreen> {
           icon: const Icon(Icons.menu, color:Color(0xFFF4F4F4)),
           onPressed: () {},
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 12.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.white24,
+              child: Icon(Icons.person, color:Color(0xFFF4F4F4)), //later: radius: 20,backgroundImage: NetworkImage(user.profileImageUrl),
+            ),
+          )
+        ],
       ),
     );
   }
