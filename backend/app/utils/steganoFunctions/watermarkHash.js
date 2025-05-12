1761f5ce032c7de9441da699ecd71577f9a416a5
Malak Atieh <malak.atieh12@gmail.com>
2025-05-12 18:01:11 +0300

    added watermarkhash

---
+++ backend/app/utils/steganoFunctions/watermarkHash.js
@@ -0,0 +1,10 @@
+class WatermarkHashEmbedder {
+  static store(ctx, hash, width, height) { 
+    
+   }
+
+  static retrieve(ctx, width, height) { 
+    
+   }
+}
+module.exports = WatermarkHashEmbedder;
