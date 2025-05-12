0220f11b5d41d3d188dd0d16adfc9e940567c25b
Malak Atieh <malak.atieh12@gmail.com>
2025-05-12 15:06:00 +0300

    added image canvas for watermarking

--- backend/app/Services/WatermarkService.js
+++ backend/app/Services/WatermarkService.js
@@ -3,5 +3,22 @@ const {createResponse} = require('../Traits/response');
 const crypto = require('crypto');
 
 class WatermarkService {
-    
+    static async addWatermark(imageBuffer, watermarkData) {
+        //load the image
+        const image = await loadImage(imageBuffer);
+
+        //create a canvas
+        const canvas = createCanvas(image.width, image.height);
+        const ctx = canvas.getContext('2d');
+
+        //draw the image on the canvas
+        ctx.drawImage(image, 0, 0);
+
+        //get image data 
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const pixels = imageData.data;
+
+        //create a digest of the image data
+        
+    }
 }
