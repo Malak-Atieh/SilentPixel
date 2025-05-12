8d9591aa13c8511490919421002971216ea720b0
Malak Atieh <malak.atieh12@gmail.com>
2025-05-13 02:29:37 +0300

    added allowed file check

--- ml-services/app.py
+++ ml-services/app.py
@@ -292,3 +292,6 @@ def load_busy_area_detector():
     
     return busy_area_detector
 
+def allowed_file(filename):
+    """Check if the file has an allowed extension."""
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
