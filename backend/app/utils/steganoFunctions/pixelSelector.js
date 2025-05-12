c204ad65652a14341ad28798d6d4a18c5f8bafb3
Malak Atieh <malak.atieh12@gmail.com>
2025-05-12 08:39:08 +0300

    editeed shuffle array

--- backend/app/utils/steganoFunctions/pixelSelector.js
+++ backend/app/utils/steganoFunctions/pixelSelector.js
@@ -88,6 +88,15 @@ class PixelSelector {
     
     return false;
   }
+
+  static _shuffleArray(array) {
+    const result = [...array];
+    for (let i = result.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [result[i], result[j]] = [result[j], result[i]];
+    }
+    return result;
+  }
   /*
     static getIndices(width, height, busyAreas = [], dataLength) {
       const indices = [];
