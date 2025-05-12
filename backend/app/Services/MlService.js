811012e05793b857eeb63fe5eb74182e4adf71f5
Malak Atieh <malak.atieh12@gmail.com>
2025-05-12 09:22:32 +0300

    added detectstego in mlservices

--- backend/app/Services/MlService.js
+++ backend/app/Services/MlService.js
@@ -6,21 +6,9 @@ class MLService {
     this.apiUrl = process.env.ML_API_URL || 'http://localhost:5000/api';
   }
 
-  async function callPythonML({ type, image, message, password, watermark, generateQR }) {
-  const formData = new FormData();
-  formData.append('image', image, 'image.png');
-  if (message) formData.append('message', message);
-  if (password) formData.append('password', password);
-  if (watermark) formData.append('watermark', watermark);
-  if (generateQR !== undefined) formData.append('generateQR', generateQR.toString());
+   async detectSteganography(imageBuffer) {
 
-  const endpoint = type === 'encode' ? '/encode' : '/decode';
+  }
 
-  const response = await axios.post(`http://localhost:5000/api/${endpoint}`, formData, {
-    headers: formData.getHeaders()
-  });
-
-  return response.data;
 }
-}
-module.exports = { callPythonML };
+module.exports = {  };
