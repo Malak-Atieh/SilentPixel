f3d545dd0baf80042c7e3852f32e56932b0467a5
Malak Atieh <malak.atieh12@gmail.com>
2025-05-14 00:00:59 +0300

    Imported image model

--- backend/app/Services/steganographyService.js
+++ backend/app/Services/steganographyService.js
@@ -2,3 +2,4 @@ const SteganoUtils = require('./SteganoUtils');
 const QRService = require('./qrService');
 const WatermarkService = require('./WatermarkService');
 const MLService = require('./MLService');
+const Image = require('../Models/Image');
