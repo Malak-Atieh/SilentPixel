8ea628db6df133cce6c9f48974917a1de2124d62
Malak Atieh <malak.atieh12@gmail.com>
2025-05-13 20:31:20 +0300

    added default timeout

--- backend/app/Services/MlService.js
+++ backend/app/Services/MlService.js
@@ -4,6 +4,7 @@ const { AppError } = require('../Traits/errors');
 class MLService {
   constructor() {
     this.apiUrl = process.env.ML_API_URL || 'http://localhost:5000/api';
+    this.defaultTimeout = 30000;
   }
 
    async detectSteganography(imageBuffer) {
