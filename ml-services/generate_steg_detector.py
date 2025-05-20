b5a8d19617afdc74cf8c5846dcc17d6c6aed2b9d
Malak Atieh <malak.atieh12@gmail.com>
2025-05-20 12:13:56 +0300

    saved modified image

--- ml-services/generate_steg_detector.py
+++ ml-services/generate_steg_detector.py
@@ -105,3 +105,5 @@ def dct_encode(image, secret_data, alpha=0.1):
     for i in range(0, h, 8):
         for j in range(0, w, 8):
             if data_index < data_len:
+                block = modified_image[i:i+8, j:j+8]
+    
