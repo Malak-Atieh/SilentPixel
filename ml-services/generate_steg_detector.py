d3b56bad0aee7e72c8e0baa14d4320e543079544
Malak Atieh <malak.atieh12@gmail.com>
2025-05-20 11:58:53 +0300

    added him

--- ml-services/generate_steg_detector.py
+++ ml-services/generate_steg_detector.py
@@ -69,4 +69,4 @@ def generate_random_text(min_length=100, max_length=500):
              "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", 
              "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", 
              "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", 
-             "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", 
+             "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", 
