dd121be7c805b09fece22189f124a87557873544
Malak Atieh <malak.atieh12@gmail.com>
2025-05-21 10:24:02 +0300

    added dropout

--- ml-services/train_steg_detector.py
+++ ml-services/train_steg_detector.py
@@ -75,7 +75,8 @@ def build_model(num_classes=3, model_name="mobilenet_v3", freeze_features=True):
         model.classifier = nn.Sequential(
             nn.Linear(in_features, 512),
             nn.Hardswish(),
- 
+            nn.Dropout(0.3),
+
     elif model_name == "efficientnet_b0":
         model = models.efficientnet_b0(weights='IMAGENET1K_V1')
         in_features = model.classifier[1].in_features
