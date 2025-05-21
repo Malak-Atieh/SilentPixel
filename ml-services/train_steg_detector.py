2490b523d1e8a494406f04857c469e04d8159405
Malak Atieh <malak.atieh12@gmail.com>
2025-05-21 10:22:39 +0300

    added if freeze layers

--- ml-services/train_steg_detector.py
+++ ml-services/train_steg_detector.py
@@ -69,6 +69,7 @@ def build_model(num_classes=3, model_name="mobilenet_v3", freeze_features=True):
     if model_name == "mobilenet_v3":
         model = models.mobilenet_v3_small(weights='IMAGENET1K_V1')
         in_features = 576
+        if freeze_features:
 
     elif model_name == "efficientnet_b0":
         model = models.efficientnet_b0(weights='IMAGENET1K_V1')
