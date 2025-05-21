f310689c02e9e67312ab05ba9d936a38777cabe5
Malak Atieh <malak.atieh12@gmail.com>
2025-05-21 10:10:39 +0300

    added torchvision

--- ml-services/train_steg_detector.py
+++ ml-services/train_steg_detector.py
@@ -4,10 +4,10 @@ import logging
 import argparse
 import numpy as np
 from pathlib import Path
+import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, models
 from PIL import Image
 import random
 from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
